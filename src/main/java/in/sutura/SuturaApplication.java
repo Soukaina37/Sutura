52e465d6c017712fd7a9fb7ac78e84301f58030f
soukaina rhasri <82758233+Soukaina37@users.noreply.github.com>
2021-05-28 01:50:52 +0100

    Premier Commit

---
+++ src/main/java/in/sutura/SuturaApplication.java
@@ -0,0 +1,13 @@
+package in.sutura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuturaApplication.class, args);
+	}
+
+}
